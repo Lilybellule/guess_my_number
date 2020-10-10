2d34d884c522f54d38a49566229c176499a16b3c
Liselotte Nesme <liselotte.nesme@epfl.ch>
2020-10-10 12:41:29 +0200

    Amélioration 2 de computer_player.py tentatives avec le nombre exactement entre min et max

--- computer_player.py
+++ computer_player.py
@@ -1,17 +1,16 @@
-import random
-
-form guess_my_number import MIN, MAX, GuessMachine
+from guess_my_number import MIN, MAX, GuessMachine
 
 if __name__ == '__main__':
     min =  MIN
     max = MAX
     guess_nachine = GuessMachine()
     while True :
-        attempt = random.randint(min,max)
+        attempt = int((min + max) /2)
         result = guess_nachine.guess(attempt)
         print('tried %d : %s' %(attempt, result))
         if result == 'found':
             print('Finished in %d attempts.' %guess_nachine.number_of_attempt)
+            break
         elif result == 'too low':
             min = attempt + 1
         else:
